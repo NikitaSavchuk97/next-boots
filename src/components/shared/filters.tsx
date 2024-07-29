7c318ddbb4e8d06e6e70106a030525b5619e6edf
Nikita Savchuk <nikita.savchuk@gmail.com>
2024-07-29 14:18:53 +0300

    Компонент: РэнджСлайдер

--- src/components/shared/filters.tsx
+++ src/components/shared/filters.tsx
@@ -1,6 +1,8 @@
 import { FC } from 'react';
 import { cn } from '@/lib/utils';
 import { FilterCheckbox, Title } from '@/components/shared';
+import { Input } from '@/components/ui';
+import { RangeSlider } from './range-slider';
 
 interface Props {
   className?: string;
@@ -13,6 +15,16 @@ export const Filters: FC<Props> = ({ className }) => {
 
       <div className='flex flex-col gap-4'>
         <FilterCheckbox text='В наличии' value='1' />
+      </div>
+
+      <div className='mt-5 border-y border-y-neutral-100 py-6 pb-7'>
+        <p className='font-bold mb-3'>Цена от и до:</p>
+        <div className='flex gap-3 mb-5'>
+          <Input type='number' placeholder='0' min={0} max={30000} defaultValue={0} />
+          <Input type='number' placeholder='30000' min={0} max={30000} defaultValue={30000} />
+        </div>
+
+        <RangeSlider min={0} max={30000} step={100} value={[0, 30000]} />
       </div>
     </div>
   );
