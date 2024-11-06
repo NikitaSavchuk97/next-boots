a061fbf5b29521286ebdfb4c0841224a88c0548a
Nikita Savchuk <nikita.savchuk@gmail.com>
2024-11-06 08:37:48 +0300

    Фикс: Карт

--- shared/components/shared/cart-drawer.tsx
+++ shared/components/shared/cart-drawer.tsx
@@ -1,5 +1,12 @@
 'use client';
 
+import Link from 'next/link';
+import Image from 'next/image';
+import { Button } from '../ui';
+import { Title } from './title';
+import { useCartStore } from '@/shared/store';
+import CartDrawerItem from './cart-drawer-item';
+import { ArrowLeft, ArrowRight } from 'lucide-react';
 import { FC, PropsWithChildren, useEffect } from 'react';
 import {
   Sheet,
@@ -10,15 +17,12 @@ import {
   SheetTitle,
   SheetTrigger,
 } from '@/shared/components/ui/sheet';
-import Link from 'next/link';
-import { Button } from '../ui';
-import { ArrowRight } from 'lucide-react';
-import CartDrawerItem from './cart-drawer-item';
-import { useCartStore } from '@/shared/store';
+import { cn } from '@/shared/lib/utils';
 
 interface Props {
   className?: string;
 }
+
 export const CartDrawer: FC<PropsWithChildren<Props>> = ({ children, className }) => {
   const [items, totalAmount, fetchCartItems, removeCartItem] = useCartStore((state) => [
     state.items,
@@ -35,59 +39,85 @@ export const CartDrawer: FC<PropsWithChildren<Props>> = ({ children, className }
     <Sheet>
       <SheetTrigger asChild>{children}</SheetTrigger>
       <SheetContent
-        className='flex flex-col justify-between pb-0 bg-gray-200'
+        className={cn(
+          'flex flex-col justify-between pb-0 bg-gray-200',
+          !totalAmount && 'justify-center',
+        )}
         aria-describedby={undefined}
       >
-        <SheetHeader>
-          <SheetTitle>
-            В корзине <span className='font-bold-'>{items.length} товара</span>
-          </SheetTitle>
-        </SheetHeader>
+        {!totalAmount && (
+          <div className='flex flex-col items-center justify-center w-72 mx-auto'>
+            <Image src='/empty-box.png' alt='Empty cart' width={120} height={120} />
+            <Title size='sm' text='Корзина пустая' className='text-center font-bold my-2' />
+            <p className='text-center text-neutral-500 mb-5'>
+              Добавьте хотя бы одну пару, чтобы совершить заказ
+            </p>
 
-        <div className='-mx-6 mt-5 overflow-auto scrollbar flex-1'>
-          {items.map((item: any) => {
-            return (
-              <div className='mb-2' key={item.id}>
-                <CartDrawerItem
-                  disabled={item.disabled}
-                  id={item.id}
-                  name={item.name}
-                  type={item.type}
-                  size={item.size}
-                  price={item.price}
-                  quantity={item.quantity}
-                  imageUrl={item.imageUrl}
-                  onClickRemove={() => removeCartItem(item.id)}
-                />
-              </div>
-            );
-          })}
-        </div>
+            <SheetClose>
+              <Button className='w-56 h-12 text-base' size='lg'>
+                <ArrowLeft className='w-5 mr-2' />
+                Вернуться назад
+              </Button>
+            </SheetClose>
+          </div>
+        )}
 
-        <SheetFooter className='-mx-6 bg-white p-8'>
-          <div className='w-full'>
-            <div className='flex mb-4'>
-              <span className='flex flex-1 text-lg text-neutral-500'>
-                Итого
-                <div className='flex-1 border-b border-dashed border-b-neutral-200 relative -top-1 mx-2' />
-              </span>
+        {totalAmount > 0 ? (
+          <>
+            <SheetHeader>
+              <SheetTitle>
+                В корзине <span className='font-bold-'>{items.length} товара</span>
+              </SheetTitle>
+            </SheetHeader>
 
-              <span className='font-bold text-lg'>{totalAmount} ₽</span>
+            <div className='-mx-6 mt-5 overflow-auto scrollbar flex-1'>
+              {items.map((item: any) => {
+                return (
+                  <div className='mb-2' key={item.id}>
+                    <CartDrawerItem
+                      disabled={item.disabled}
+                      id={item.id}
+                      name={item.name}
+                      type={item.type}
+                      size={item.size}
+                      price={item.price}
+                      quantity={item.quantity}
+                      imageUrl={item.imageUrl}
+                      onClickRemove={() => removeCartItem(item.id)}
+                    />
+                  </div>
+                );
+              })}
             </div>
 
-            <Link href='/cart'>
-              <Button
-                //onClick={() => setRedirecting(true)}
-                //loading={loading || redirecting}
-                type='submit'
-                className='w-full h-12 text-base'
-              >
-                Оформить заказ
-                <ArrowRight className='w-5 ml-2' />
-              </Button>
-            </Link>
-          </div>
-        </SheetFooter>
+            <SheetFooter className='-mx-6 bg-white p-8'>
+              <div className='w-full'>
+                <div className='flex mb-4'>
+                  <span className='flex flex-1 text-lg text-neutral-500'>
+                    Итого
+                    <div className='flex-1 border-b border-dashed border-b-neutral-200 relative -top-1 mx-2' />
+                  </span>
+
+                  <span className='font-bold text-lg'>{totalAmount} ₽</span>
+                </div>
+
+                <Link href='/cart'>
+                  <Button
+                    //onClick={() => setRedirecting(true)}
+                    //loading={loading || redirecting}
+                    type='submit'
+                    className='w-full h-12 text-base'
+                  >
+                    Оформить заказ
+                    <ArrowRight className='w-5 ml-2' />
+                  </Button>
+                </Link>
+              </div>
+            </SheetFooter>
+          </>
+        ) : (
+          <></>
+        )}
       </SheetContent>
     </Sheet>
   );
