1c9ff041924112d63877e85d39e5b5a39c4986a5
Nikita Savchuk <nikita.savchuk@gmail.com>
2024-11-28 09:10:05 +0300

    Фикс: ЧекаутКарт

--- shared/components/shared/checkout-components/checkout-cart.tsx
+++ shared/components/shared/checkout-components/checkout-cart.tsx
@@ -13,25 +13,22 @@ interface PropTypes {
 export const CheckoutCart: FC<PropTypes> = ({ className, items, removeCartItem, loading }) => {
   return (
     <WhiteBlock title='1. Корзина' className={className}>
-      {loading &&
-        [...Array(2)].map((_, index) => <Skeleton key={index} className='h-[89px] m-5' />)}
-
-      {!loading &&
-        items.length > 0 &&
-        items.map((item: any) => (
-          <CartDrawerItem
-            key={item.id}
-            disabled={item.disabled}
-            id={item.id}
-            imageUrl={item.imageUrl}
-            name={item.name}
-            price={item.price}
-            quantity={item.quantity}
-            type={item.type}
-            size={item.size}
-            onClickRemove={() => removeCartItem(item.id)}
-          />
-        ))}
+      {loading
+        ? [...Array(2)].map((_, index) => <Skeleton key={index} className='h-[89px] m-5' />)
+        : items.map((item: any) => (
+            <CartDrawerItem
+              key={item.id}
+              disabled={item.disabled}
+              id={item.id}
+              imageUrl={item.imageUrl}
+              name={item.name}
+              price={item.price}
+              quantity={item.quantity}
+              type={item.type}
+              size={item.size}
+              onClickRemove={() => removeCartItem(item.id)}
+            />
+          ))}
     </WhiteBlock>
   );
 };
